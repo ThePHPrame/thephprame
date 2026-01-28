5758e98ecd5a0fdfddf34127d64db76ed6fde810
Wesley Middelbos <wesley.middelbos@cern.ch>
2026-01-28 21:38:33 +0100

    Fix file path error

--- Config/database.php
+++ Config/database.php
@@ -1,16 +1,5 @@
 <?php
-require_once "./Config/app.php";
-
-function env_value($key, $default = null) {
-    if (array_key_exists($key, $_ENV)) {
-        return $_ENV[$key];
-    }
-    $value = getenv($key);
-    if ($value !== false) {
-        return $value;
-    }
-    return $default;
-}
+require_once __DIR__ . "/app.php";
 
 // Check configuration options: https://book.cakephp.org/phinx/0/en/contents.html
 $db_config = [
@@ -18,13 +7,13 @@ $db_config = [
         'default_migration_table' => 'phinxlog',
         'default_environment' => APP_ENV,
         'production' => [
-            'adapter' => env_value('DB_ADAPTER', 'pgsql'),
-            'host' => env_value('DB_HOST', 'localhost'),
-            'name' => env_value('DB_NAME', ''),
-            'user' => env_value('DB_USER', ''),
-            'pass' => env_value('DB_PASS', ''),
-            'port' => env_value('DB_PORT', null),
-            'charset' => env_value('DB_CHARSET', 'utf8'),
+            'adapter' => $_ENV['DB_ADAPTER'] ?? 'pgsql',
+            'host' => $_ENV['DB_HOST'] ?? 'localhost',
+            'name' => $_ENV['DB_NAME'] ?? '',
+            'user' => $_ENV['DB_USER'] ?? '',
+            'pass' => $_ENV['DB_PASS'] ?? '',
+            'port' => $_ENV['DB_PORT'] ?? null,
+            'charset' => $_ENV['DB_CHARSET'] ?? 'utf8',
         ],
         'development' => [
             'adapter' => 'mysql',
